06ba480ecee520beb8681bbd295e5ce84916d9dd
harininarayan <harini.s.narayan@gmail.com>
2021-09-23 20:20:55 -0500

    adding all milestone 2 code

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,9 +2,60 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickAdd(View view) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        int num1 = Integer.parseInt(number1.getText().toString());
+        int num2 = Integer.parseInt(number2.getText().toString());
+        int answer = num1 + num2;
+
+        goToActivity2(answer);
+    }
+
+    public void clickSub(View view) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        int num1 = Integer.parseInt(number1.getText().toString());
+        int num2 = Integer.parseInt(number2.getText().toString());
+        int answer = num1 - num2;
+
+        goToActivity2(answer);
+    }
+
+    public void clickMult(View view) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        int num1 = Integer.parseInt(number1.getText().toString());
+        int num2 = Integer.parseInt(number2.getText().toString());
+        int answer = num1 * num2;
+
+        goToActivity2(answer);
+    }
+
+    public void clickDiv(View view) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        int num1 = Integer.parseInt(number1.getText().toString());
+        int num2 = Integer.parseInt(number2.getText().toString());
+        int answer = num1 / num2;
+
+        goToActivity2(answer);
+    }
+
+    public void goToActivity2(int answer){
+        String message = String.valueOf(answer);
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("intAnswer",message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
